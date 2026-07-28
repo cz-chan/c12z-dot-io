d128fc3c5f386de3a52e40545173447fdface491
Chema <mrjark7@gmail.com>
2026-07-28 19:00:15 +0200

    updated behavior page

--- src/global/site-info.ts
+++ src/global/site-info.ts
@@ -3,7 +3,7 @@ import type {
 	SiteDefaultConfigInterface,
 } from "@interfaces/siteInfo.interface.ts";
 
-export const SITE_VERSION = "v1.10.00";
+export const SITE_VERSION = "v1.10.01";
 
 export const SITE_DEFAULT_CONFIG: SiteDefaultConfigInterface = {
 	title: "Chema Ferrandez - c12z",
